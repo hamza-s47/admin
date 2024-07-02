cc3a3fe9bf20a576d90dbb7ca95a1c5e3d5d38b3
Hamza Siddiqui <hamza@technomites.com>
2024-07-02 13:10:09 +0500

    .

--- main.py
+++ main.py
@@ -6,15 +6,3 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"HW":"Hello World!"}
-
-@app.get("/rps")
-def abc():
-    Result = ""
-    
-    if rps.scorePerson > rps.scoreComp:
-        Result = "Person won!"
-    elif rps.scoreComp > rps.scorePerson:
-        Result = "Computer won!"
-    else: Result = "Tied!"
-    
-    return {"Computer Score" : rps.scoreComp, "Person Score" : rps.scorePerson, "Result" : Result}
